289db1c4024f53e8481e120a789c3ed09629b84e
Ziyue Feng <brother_yue@stu.xjtu.edu.cn>
2019-10-10 17:26:41 -0400

    Delete question1.py

--- question1.py
+++
@@ -1,9 +0,0 @@
-def solution(list, num): 
-    a=0 
-    b=0 
-    '''type in your solution, find a and b in array that a+b=num'''
-    return a, b 
-  
-numbers = [0, 2, 11, 19, 90] 
-print(solution(numbers, 21)) 
-print(solution(numbers, 25)) 
