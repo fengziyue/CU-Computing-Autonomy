69e005b25d014660dcd5f2049e62717b091f9aab
Ziyue Feng <brother_yue@stu.xjtu.edu.cn>
2019-10-10 17:31:44 -0400

    Add files via upload

---
+++ Homework2/Question3.py
@@ -0,0 +1,13 @@
+G = {1:{1:0, 2:1, 3:12},
+  2:{2:0, 3:9, 4:3},
+  3:{3:0, 5:5},
+  4:{3:4, 4:0, 5:13, 6:15},
+  5:{5:0, 6:4},
+  6:{6:0}}
+
+def Dijkstra(G,v0,INF=999):
+  '''type in your code'''
+  return dis
+
+dis = Dijkstra(G,v0=1)
+print (dis)
