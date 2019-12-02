d0c35a51c5c141d3938f4ff41bc5a9ce2c3aeb51
Ziyue Feng <zfeng@clemson.edu>
2019-12-02 09:56:08 -0500

    Create base.py

---
+++ Homework1/base.py
@@ -0,0 +1,103 @@
+import models
+import os
+import torch
+from torch import optim, nn
+from torchvision import transforms, datasets
+import torchvision
+import common
+
+#DIRECTORY SETTINGS
+os.chdir("..")#Go up two directories
+DATA_DIR = 'data/bullying'
+SAVE_DIR = 'models'
+MODEL_SAVE_PATH = os.path.join(SAVE_DIR, 'base.pt')
+
+
+#HYPERPARAMETERS
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+EPOCHS=100
+BATCH_SIZE = 32
+criterion = nn.CrossEntropyLoss()
+ADAM_OPTIMISER=True
+LEARNING_RATE=0.001
+
+
+
+#1. DATA INPUT
+train_transforms = transforms.Compose([# Data Transforms
+                           transforms.Resize(256),#Resize
+                           transforms.RandomHorizontalFlip(30),#Flip
+                           transforms.RandomRotation(10),#Roatate
+                           transforms.RandomCrop(256),#Crop
+                           transforms.ToTensor(),#Convert to Tensor
+                           transforms.Normalize((0.49139968, 0.48215827, 0.44653124), (0.24703233, 0.24348505, 0.26158768))#Normalize
+                       ])
+
+test_transforms = transforms.Compose([
+                           transforms.ToTensor(),
+                           transforms.Normalize((0.49139968, 0.48215827, 0.44653124), (0.24703233, 0.24348505, 0.26158768))
+                       ])
+
+
+
+#Directory import
+# train_data = datasets.ImageFolder(os.path.join(DATA_DIR, 'train'), train_transforms)
+# train_data, valid_data = torch.utils.data.random_split(train_data, [int(len(train_data)*0.9), len(train_data) - int(len(train_data)*0.9)])
+# test_data = datasets.ImageFolder(os.path.join(DATA_DIR, 'test'), test_transforms)
+
+#Existing Dataset Import
+train_data = torchvision.datasets.CIFAR10(root='data', train=True, download=True, transform=train_transforms)#Use CIFAR10 to train
+train_data, valid_data = torch.utils.data.random_split(train_data, [int(len(train_data)*0.9), len(train_data) - int(len(train_data)*0.9)])
+test_data = torchvision.datasets.CIFAR10(root='data', train=False, download=True, transform=test_transforms)
+
+
+print(f'Number of training examples: {len(train_data)}')
+print(f'Number of validation examples: {len(valid_data)}')
+print(f'Number of testing examples: {len(test_data)}')
+
+
+train_iterator = torch.utils.data.DataLoader(train_data, shuffle=True, batch_size=BATCH_SIZE)
+valid_iterator = torch.utils.data.DataLoader(valid_data, batch_size=BATCH_SIZE)
+test_iterator = torch.utils.data.DataLoader(test_data, batch_size=BATCH_SIZE)
+
+
+
+#2. ANALYSIS
+# model=models.AlexNet().to(device)
+# model=torchvision.models.AlexNet()
+model = torchvision.models.resnet18(pretrained=True)#TorchVision
+
+for param in model.parameters():
+    param.requires_grad = False
+num_ftrs = model.fc.in_features
+model.fc = nn.Linear(num_ftrs, 10)
+model = model.to(device)
+
+#Hyperparameters
+if(ADAM_OPTIMISER):
+    optimizer = optim.Adam(model.parameters(), lr=LEARNING_RATE)
+else:
+    optimizer = optim.SGD(model.classifier.parameters(), lr=0.001, momentum=0.5)
+
+#Train
+best_valid_loss = float('inf')
+for epoch in range(EPOCHS):#Range of Epochs
+    print(epoch)
+    train_loss, train_acc = common.train(model, device, train_iterator, optimizer, criterion)#Train Loss Calculation
+    valid_loss, valid_acc = common.evaluate(model, device, valid_iterator, criterion)#Validation Loss Calculation
+
+    if valid_loss < best_valid_loss:#Validation Loss - Is current lower than the saved validation loss.
+        best_valid_loss = valid_loss#Save the best loss (lowest)
+        torch.save(model.state_dict(), MODEL_SAVE_PATH)#Save the model
+
+    print(f'| Epoch: {epoch+1:02} | Train Loss: {train_loss:.3f} | Train Acc: {train_acc*100:05.2f}% | Val. Loss: {valid_loss:.3f} | Val. Acc: {valid_acc*100:05.2f}% |')
+
+
+
+
+#3. OUTPUT
+
+model.load_state_dict(torch.load(MODEL_SAVE_PATH)) #Load best weights from file
+test_loss, test_acc = common.evaluate(model, device, valid_iterator, criterion) #Test Loss is dependent on
+print(f'| Test Loss: {test_loss:.3f} | Test Acc: {test_acc*100:05.2f}%')
