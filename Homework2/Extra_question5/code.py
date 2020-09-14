09d6609ab82fb45c402e91a986ae0dc7f501cb10
Ziyue Feng <zfeng@clemson.edu>
2020-09-14 16:06:51 -0400

    Create code.py

---
+++ Homework2/Extra_question5/code.py
@@ -0,0 +1,21 @@
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt
+
+img = cv2.imread('messi5.jpg',0)
+img2 = img.copy()
+template = cv2.imread('template.jpg',0)
+w, h = template.shape[::-1]
+
+# All the 6 methods for comparison in a list
+methods = ['cv2.TM_CCOEFF', 'cv2.TM_CCOEFF_NORMED', 'cv2.TM_CCORR',
+            'cv2.TM_CCORR_NORMED', 'cv2.TM_SQDIFF', 'cv2.TM_SQDIFF_NORMED']
+
+for meth in methods:
+    img = img2.copy()
+    method = eval(meth)
+
+    # Apply template Matching
+    
+    
+    plt.show()
