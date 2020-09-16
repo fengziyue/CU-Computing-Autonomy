018e838b3cb42298533bb33c4fd8e4e4afc0751d
Ziyue Feng <zfeng@clemson.edu>
2020-09-16 12:41:30 -0400

    Create Question8.py

---
+++ Homework2/Question8.py
@@ -0,0 +1,17 @@
+from matplotlib import pyplot as plt
+import numpy as np 
+import scipy
+
+#Frequency in terms of Hertz
+fre  = 5 
+#Sample rate
+fre_samp = 50
+t = np.linspace(0, 2, 2 * fre_samp, endpoint = False )
+a = np.sin(fre  * 2 * np.pi * t)
+figure, axis = plt.subplots()
+axis.plot(t, a)
+axis.set_xlabel ('Time (s)')
+axis.set_ylabel ('Signal amplitude')
+plt.show()
+
+#do DFT and visualize:
