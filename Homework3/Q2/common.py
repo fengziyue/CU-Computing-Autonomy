bffd089117d3cb2532867d9c209f21ac02c18628
Ziyue Feng <zfeng@clemson.edu>
2020-10-14 10:12:34 -0400

    hw3 q2

---
+++ Homework3/Q2/common.py
@@ -0,0 +1,57 @@
+import torch
+
+
+
+def train(model, device, iterator, optimizer, criterion):
+    epoch_loss = 0
+    epoch_acc = 0
+
+    model.train()
+
+    for (x, y) in iterator:
+        x = x.to(device)
+        y = y.to(device)
+
+        optimizer.zero_grad()
+
+        fx = model(x)
+
+        loss = criterion(fx, y)
+
+        acc = calculate_accuracy(fx, y)
+
+        loss.backward()
+
+        optimizer.step()
+
+        epoch_loss += loss.item()
+        epoch_acc += acc.item()
+
+    return epoch_loss / len(iterator), epoch_acc / len(iterator)
+
+
+
+def evaluate(model, device, iterator, criterion):
+    epoch_loss = 0
+    epoch_acc = 0
+
+    model.eval()
+
+    with torch.no_grad():
+        for (x, y) in iterator:
+            x = x.to(device)
+            y = y.to(device)
+            fx = model(x)
+            loss = criterion(fx, y)
+            acc = calculate_accuracy(fx, y)
+            epoch_loss += loss.item()
+            epoch_acc += acc.item()
+    return epoch_loss / len(iterator), epoch_acc / len(iterator)
+
+
+def calculate_accuracy(fx, y):
+    preds = fx.max(1, keepdim=True)[1]
+    correct = preds.eq(y.view_as(preds)).sum()
+    acc = correct.float()/preds.shape[0]
+    return acc
+
